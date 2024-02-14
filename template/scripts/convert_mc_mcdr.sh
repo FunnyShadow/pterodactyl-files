2813a6364b40fba0804151195689292185dab695
FunnyShadow <noreply@maintest233.top>
2024-02-14 14:09:24 +0800

    修复了一个小问题

--- template/scripts/convert_mc_mcdr.sh
+++ template/scripts/convert_mc_mcdr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for eggs in "archlight" "alpine"; do
+while IFS= read -r eggs; do
   tag="egg-${eggs}"
   input="./eggs/json/minecraft/mcdr/${tag}.json"
   output="./eggs/yml/minecraft/mcdr/${tag}.yml"
-  python3 tool.py y2j -i ${input} -o ${output}
-done
+  python3 tool.py y2j -i "${input}" -o "${output}"
+done < egglist
