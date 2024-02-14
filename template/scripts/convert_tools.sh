dd7ecbd05e61e3dbb265de35612696e10d3b47dc
FunnyShadow <noreply@maintest233.top>
2024-02-14 13:57:28 +0800

    实现: 从文件导入需转换的 egg 列表

--- template/scripts/convert_tools.sh
+++ template/scripts/convert_tools.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for eggs in "archlight" "alpine"; do
+while IFS= read -r eggs; do
   tag="egg-${eggs}"
   input="./eggs/json/tools/${tag}.json"
   output="./eggs/yml/tools/${tag}.yml"
-  python3 tool.py y2j -i ${input} -o ${output}
-done
+  python3 tool.py y2j -i "${input}" -o "${output}"
+done < egglist
