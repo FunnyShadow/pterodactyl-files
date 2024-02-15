8f8547f82c88daa8142ade5699700b047ce74c40
FunnyShadow <noreply@maintest233.top>
2024-02-15 18:37:59 +0800

    （？

--- template/scripts/j2y/convert_tools.sh
+++ template/scripts/j2y/convert_tools.sh
@@ -2,12 +2,7 @@
 
 while IFS= read -r eggs; do
   tag="egg-${eggs}"
-<<<<<<< HEAD
   input="./eggs/json/tools/${tag}.json"
   output="./eggs/yml/tools/${tag}.yml"
-=======
-  input="./eggs/json/tools/${tag}.yml"
-  output="./eggs/yml/tools/${tag}.json"
->>>>>>> 1cb7adf1ae224aec2d6de6b63e7e59e9e629fc23
   python3 tool.py y2j -i "${input}" -o "${output}"
 done < egglist
--- template/scripts/y2j/convert_mc_mcdr.sh
+++ template/scripts/y2j/convert_mc_mcdr.sh
@@ -2,12 +2,7 @@
 
 while IFS= read -r eggs; do
   tag="egg-${eggs}"
-<<<<<<< HEAD
   input="./eggs/json/minecraft/mcdr/${tag}.yml"
   output="./eggs/yml/minecraft/mcdr/${tag}.json"
-=======
-  input="./eggs/json/minecraft/mcdr/${tag}.json"
-  output="./eggs/yml/minecraft/mcdr/${tag}.yml"
->>>>>>> 1cb7adf1ae224aec2d6de6b63e7e59e9e629fc23
   python3 tool.py y2j -i "${input}" -o "${output}"
 done < egglist
