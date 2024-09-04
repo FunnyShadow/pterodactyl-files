9853761977a1440ac96d066abda953d85e003df2
FunnyShadow <admin@bluefunny.email>
2024-09-05 00:27:37 +0800

    Optimized code

--- docker/runtime/build/entrypoint.sh
+++ docker/runtime/build/entrypoint.sh
@@ -1,11 +1,24 @@
-#!/bin/bash
-set -e
-cd /home/container
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Change to the container's home directory
+cd /home/container || exit 1
+
+# Function to log messages
+log() {
+    echo "[Pterodactyl Daemon]: $*"
+}
 
 # Replace Startup Variables
-STARTUP_CMD=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
-STARTUP_EXPANDED=$(echo "$STARTUP_CMD" | envsubst)
-echo ":$(pwd)$ ${STARTUP_EXPANDED}"
+STARTUP_CMD=${STARTUP//\{\{/\$\{} # Replace '{{' with '${'
+STARTUP_CMD=${STARTUP_CMD//\}\}/\}} # Replace '}}' with '}'
+
+# Expand environment variables
+STARTUP_EXPANDED=$(eval echo "$STARTUP_CMD")
+
+log "Current directory: $(pwd)"
+log "Expanded startup command: $STARTUP_EXPANDED"
 
 # Run the Server
-eval "${STARTUP_EXPANDED}"
+eval "$STARTUP_EXPANDED"
